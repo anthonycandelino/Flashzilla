f4114b982b77f4c43f411305e122b17874a8042d
Anthony Candelino <anthcandelino@gmail.com>
2024-10-08 12:44:23 -0400

    Add initial app code

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,16 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Anthony Candelino on 2024-10-01.
+//
+
+import Foundation
+
+struct Card: Codable {
+    var prompt: String
+    var answer: String
+    
+    static let example = Card(prompt: "What is the capital of France?", answer: "Paris")
+
+}
